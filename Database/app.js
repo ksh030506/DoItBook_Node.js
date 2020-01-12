86ee7ea2445a673eb11f946173b26664e44f5256
sanghyun0 <sanghyun0@github.com>
2020-01-12 15:18:55 +0900

    htiti

---
+++ Database/app.js
@@ -0,0 +1,48 @@
+var express = require('express');
+var http = require('http');
+var static = require('serve-static');
+var path = require('path');
+
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var expressSession = require('express-session');
+
+//익스프레스 에러 핸들어 모듈 사용
+var expressErrorHandler = require('express-error-handler');
+
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use('/public', static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.use(cookieParser());
+app.use(expressSession({
+    secret:'my key',
+    resave:true,
+    saveUninitialized:true
+}));
+
+
+var router = express.Router();
+
+
+
+
+app.use('/', router);
+var errorHandler = expressErrorHandler({
+    static: {
+        '404': './public/404.html'
+    }
+});
+
+app.use( expressErrorHandler.httpError(404) );
+app.use( errorHandler );
+
+
+var server = http.createServer(app).listen(app.get('port'), function(){
+    console.log('익스프레스로 웹 서버를 실행함' + app.get('port'));
+});
